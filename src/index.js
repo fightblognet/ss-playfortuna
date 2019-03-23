c710d751f05e5e7e1bd632c4282fb3fdb11f3e8c
Sergey Bakhar <b113.bahar@gmail.com>
2019-03-23 18:58:41 +0200

    initial commit

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import './scss/main.scss';
+import alert from './js/main';
+
+alert();
+console.log('hello1');
---
+++ src/js/main.js
@@ -0,0 +1,5 @@
+const alert = () => {
+    console.log('foo')
+}
+
+export default alert;
